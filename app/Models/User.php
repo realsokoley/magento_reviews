786433338d368ef3cae70d973ca89cb0b22deca1
alekseysokolov <sokoley@yandex.ru>
2023-03-26 21:08:08 +0300

    Users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property integer $id
+ * @property string $username
+ * @property string $name
+ * @property string $email
+ * @property string $email_verified_at
+ * @property string $password
+ * @property string $api_token
+ * @property string $remember_token
+ * @property string $created_at
+ * @property string $updated_at
+ * @property integer $paid
+ * @property integer $active
+ * @property string $payment_date
+ * @property string $last_game_time
+ * @property integer $games_left
+ * @property GameUser[] $gameUsers
+ * @property PrivateRoom[] $privateRooms
+ * @property Rating[] $ratings
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
+     * @var array
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['username', 'name', 'email', 'email_verified_at', 'password', 'api_token', 'remember_token', 'created_at', 'updated_at', 'paid', 'active', 'payment_date'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
--- database/migrations/2023_02_11_165536_create_users_table.php
+++ database/migrations/2023_02_11_165536_create_users_table.php
@@ -8,24 +8,34 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->comment('');
+            $table->integer('id', true);
+            $table->string('username')->unique('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('api_token', 80)->nullable()->unique();
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('paid')->default(0);
+            $table->integer('active')->default(0);
+            $table->timestamp('payment_date')->nullable();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
