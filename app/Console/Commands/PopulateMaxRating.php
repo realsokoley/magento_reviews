b165975ae19894eef96ed5033254c8d906f121f3
alekseysokolov <sokoley@yandex.ru>
2023-05-29 12:28:00 +0300

    Rating upd

--- app/Console/Commands/PopulateMaxRating.php
+++ app/Console/Commands/PopulateMaxRating.php
@@ -44,9 +44,9 @@ class PopulateMaxRating extends Command
                 if (is_array($taskData)) {
                     if (isset($taskData[0]) && is_array($taskData[0])) {
                         if (isset($taskData[0][0]) && is_array($taskData[0][0])) {
-                            $maxRating = count($taskData[0]);
-                        } else {
                             $maxRating = count($taskData);
+                        } else {
+                            $maxRating = count($taskData[0]);
                         }
                     } else {
 
