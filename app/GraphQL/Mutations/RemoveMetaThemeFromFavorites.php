affa2982cde71844b5911f61c4506793934569d8
alekseysokolov <sokoley@yandex.ru>
2023-06-08 00:45:26 +0300

    Favorites

--- app/GraphQL/Mutations/RemoveMetaThemeFromFavorites.php
+++ app/GraphQL/Mutations/RemoveMetaThemeFromFavorites.php
@@ -4,16 +4,12 @@ namespace App\GraphQL\Mutations;
 
 use App\Models\UserFavoriteMetaTopic;
 use GraphQL\Type\Definition\ResolveInfo;
-use Illuminate\Support\Facades\Log;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class RemoveMetaThemeFromFavorites
 {
     public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        Log::info('User ID: '.$context->user()->id);
-        Log::info('Meta Topic ID: '.$args['meta_topic_id']);
-
         $userFavoriteTopic = UserFavoriteMetaTopic::where(
             [
                 ['user_id', '=', $context->user()->id],
@@ -21,13 +17,8 @@ class RemoveMetaThemeFromFavorites
             ]
         )->first();
 
-        Log::info('User Favorite Meta Topic: '.($userFavoriteTopic ? 'Found' : 'Not Found'));
+        $userFavoriteTopic?->delete();
 
-        if ($userFavoriteTopic) {
-            $userFavoriteTopic->delete();
-            Log::info('User Favorite Meta Topic deleted.');
-        } else {
-            Log::info('User Favorite Meta Topic not deleted because it was not found.');
-        }
+        return 'ok';
     }
 }
