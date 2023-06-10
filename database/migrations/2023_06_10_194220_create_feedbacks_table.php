fed812f0d786dab545fef589833343299eae0fa0
alekseysokolov <sokoley@yandex.ru>
2023-06-10 22:44:43 +0300

    Feedbacks

--- database/migrations/2023_06_10_194220_create_feedbacks_table.php
+++ database/migrations/2023_06_10_194220_create_feedbacks_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('feedbacks', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('email');
+            $table->text('message');
             $table->timestamps();
         });
     }
