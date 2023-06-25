8ca2bbc7d8a217b52bce49bac3246176df90c161
realsokoley <sokoley1993@gmail.com>
2023-06-25 08:12:28 +0000

    Support page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>@yield('title')</title>
+    <!-- Optional metadata, CSS and JS here -->
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
+</html>
+
---
+++ resources/views/support/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>App Support Page</h1>
+    <p>Welcome to the support page. Here you can find help and answers to your questions.</p>
+@endsection
+
