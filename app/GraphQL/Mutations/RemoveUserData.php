d70970bdc6149666772f7a88ca6f88aa6121644d
alekseysokolov <sokoley@yandex.ru>
2023-06-27 19:32:39 +0300

    Remove User Data

--- app/GraphQL/Mutations/RemoveUserData.php
+++ app/GraphQL/Mutations/RemoveUserData.php
@@ -7,8 +7,6 @@ use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class RemoveUserData
 {
-    private array $wordLists = [];
-
     public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         $user = $context->user();
